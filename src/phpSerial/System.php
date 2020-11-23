b1bbe010bf1cf8f7887a056141f4cda46cc04bf4
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2020-11-23 21:19:01 +0100

    Shutdown functions not working, removed
    Not sure if we actually need it.

--- src/phpSerial/System.php
+++ src/phpSerial/System.php
@@ -12,36 +12,26 @@ class System
     public function __construct()
     {
         setlocale(LC_ALL, 'en_US');
-
         $sysName = php_uname();
 
         if (strpos($sysName, 'Linux') === 0) {
             $this->operatingSystem = 'linux';
             $this->_os = 'linux';
 
-            if (exec("stty") === 0) {
-                $this->shutdown();
-            } else {
+            if (exec("stty") !== 0) {
                 throw new Exception(
                     'No stty available, unable to run.'
                 );
             }
         } elseif (strpos($sysName, 'Darwin') === 0) {
             $this->_os = 'osx';
-            $this->shutdown();
         } elseif (strpos($sysName, 'Windows') === 0) {
             $this->_os = 'windows';
-            $this->shutdown();
         } else {
             trigger_error(
                 'Unknown host OS, unable to run.',
                 E_USER_ERROR
             );
         }
-    }
-
-    private function shutdown(): void
-    {
-        register_shutdown_function(array($this, 'deviceClose'));
     }
 }
